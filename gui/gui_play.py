8f669e7a28fc0f342ef9caec011132958d96b57f
ColgateSmile <mor.dror131@gmail.com>
2021-08-20 22:39:36 +0300

    started a few classess for Gui component

---
+++ gui/gui_play.py
@@ -0,0 +1,95 @@
+import pygame
+from gui.Grid_Class import Grid
+from solvers import naive_solver
+import time
+
+
+def redraw_window(win, board, timeCounter, strikes):
+    win.fill((255, 255, 255))
+    # Draw time
+    fnt = pygame.font.SysFont("comicsans", 40)
+    text = fnt.render("Time: " + format_time(timeCounter), 1, (0, 0, 0))
+    win.blit(text, (540 - 160, 560))
+    # Draw Strikes
+    text = fnt.render("X " * strikes, 1, (255, 0, 0))
+    win.blit(text, (20, 560))
+    # Draw grid and board
+    board.draw(win)
+
+
+def format_time(secs) -> str:
+    sec = secs % 60
+    minute = secs // 60
+    hour = minute // 60
+
+    mat = " " + str(hour) + ":" + str(minute) + ":" + str(sec)
+    return mat
+
+
+def main():
+    win = pygame.display.set_mode((540, 600))
+    pygame.display.set_caption("Sudoku")
+    board = Grid(9, 9, 540, 540)
+    key = None
+    run = True
+    start = time.time()
+    strikes = 0
+    while run:
+
+        play_time = round(time.time() - start)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    key = 1
+                if event.key == pygame.K_2:
+                    key = 2
+                if event.key == pygame.K_3:
+                    key = 3
+                if event.key == pygame.K_4:
+                    key = 4
+                if event.key == pygame.K_5:
+                    key = 5
+                if event.key == pygame.K_6:
+                    key = 6
+                if event.key == pygame.K_7:
+                    key = 7
+                if event.key == pygame.K_8:
+                    key = 8
+                if event.key == pygame.K_9:
+                    key = 9
+                if event.key == pygame.K_DELETE:
+                    board.clear()
+                    key = None
+                if event.key == pygame.K_RETURN:
+                    i, j = board.selected
+                    if board.cubes[i][j].temp != 0:
+                        if board.place(board.cubes[i][j].temp):
+                            print("Success")
+                        else:
+                            print("Wrong")
+                            strikes += 1
+                        key = None
+
+                        if board.is_finished():
+                            print("Game over")
+                            run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                clicked = board.click(pos)
+                if clicked:
+                    board.select(clicked[0], clicked[1])
+                    key = None
+
+        if board.selected and key != None:
+            board.sketch(key)
+
+        redraw_window(win, board, play_time, strikes)
+        pygame.display.update()
+
+
+main()
+pygame.quit()
---
+++ tests/Sudoku_test.py
@@ -0,0 +1,23 @@
+import unittest
+import numpy as np
+import sudoku
+
+test_board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+]
+
+
+class Sudoku_test(unittest.TestCase):
+    @staticmethod
+    def test_Sudoku():
+        SudokuTest = sudoku.Sudoku(np.array(test_board))
+        SudokuTest.print_board()
+        SudokuTest.get_value(pos=(3, 2))
