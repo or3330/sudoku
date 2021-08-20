f18ad8a2ff204771eff3503dc9b92d2512fbe11d
אור קרן <or-hai-keren@campus.technion.ac.il>
2021-08-20 15:44:10 +0300

    wise solver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from solvers import NaiveSolver
+from solvers import NaiveSolver, WiseSolver
 from loaders import CsvLoader
+from copy import deepcopy
 
 board = [
     [7, 8, 0, 4, 0, 0, 1, 2, 0],
@@ -16,9 +17,22 @@ board = [
 
 if __name__ == '__main__':
     sudoku, solved_sudoku = CsvLoader().load()
+    saved_sudoku = deepcopy(sudoku)
     sudoku.print_board()
     naive_solver = NaiveSolver(sudoku=sudoku)
-    naive_solver.run()
+    naive_time = naive_solver.run_with_time_analysis()
     print("\n---------------------------------------------------------------------------------\n")
     sudoku.print_board()
     print("\n---------------------------------------------------------------------------------\n")
+
+    sudoku = saved_sudoku
+    sudoku.print_board()
+    wise_solver = WiseSolver(sudoku=sudoku)
+    wise_time = wise_solver.run_with_time_analysis()
+    print("\n---------------------------------------------------------------------------------\n")
+    sudoku.print_board()
+    print("\n---------------------------------------------------------------------------------\n")
+
+    print(f'Wise solver time: {wise_time} seconds')
+    print(f'Naive solver time: {naive_time} seconds')
+
